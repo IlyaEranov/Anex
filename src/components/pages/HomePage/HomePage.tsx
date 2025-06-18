528b50701ddcdb8871aea0fecd6be9e53fdcd996
Ilya Eranov <ilya.erannov123454321@gmail.com>
2025-06-18 17:00:13 +0500

    feat: adding bottom feedback

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,22 +1,37 @@
 import Layout from "../../common/Layout/Layout"
 import AboutSection from "../../ui/HomePage/sections/AboutSection/AboutSection"
-import FeedbackSection from "../../ui/HomePage/sections/FeedbackSection/FeedbackSection"
+import FeedbackSection from "../../ui/sections/FeedbackSection/FeedbackSection"
 import HeroSection from "../../ui/HomePage/sections/HeroSection/HeroSection"
 import HotelSection from "../../ui/HomePage/sections/HotelsSection/HotelSection"
 import NewsSection from "../../ui/HomePage/sections/NewsSection/NewsSection"
 import ReviewSection from "../../ui/HomePage/sections/ReviewSection/ReviewSection"
 import ToursSection from "../../ui/HomePage/sections/ToursSection/ToursSection"
+import image1 from "../../../assets/img/8e5857643caecfce77e4bdb1d01bc990b742b63d.jpg"
+import image2 from "../../../assets/img/3471455ca3fc71aa3a510f2390b927e3028b81d0.jpg"
+import s from "./HomePage.module.scss"
 
-function HomePage(){
-  return(
+function HomePage() {
+  return (
     <Layout>
-      <HeroSection/>
-      <ToursSection/>
-      <HotelSection/>
-      <FeedbackSection/>
-      <ReviewSection/>
-      <NewsSection/>
-      <AboutSection/>
+      <HeroSection />
+      <ToursSection />
+      <HotelSection />
+      <FeedbackSection
+        image={image1}
+        h2="НЕ ОПРЕДЕЛИЛИСЬ С ВЫБОРОМ?"
+        h3="Оставьте свой номер и наш специалист поможет вам с подбором тура"
+      />
+      <ReviewSection />
+      <NewsSection />
+      <AboutSection />
+      <div className={s.bottom}>
+        <FeedbackSection
+          image={image2}
+          h2="МЫ ПОМОЖЕМ СОЗДАТЬ ВАШЕ ПУТЕШЕСТВИЕ"
+          h3="Оставьте заявку на подбор идеального путешествия"
+          center
+        />
+      </div>
     </Layout>
   )
 }
---
+++ src/components/ui/sections/FeedbackSection/FeedbackSection.tsx
@@ -0,0 +1,34 @@
+import Button from "../../../common/buttons/Button/Button"
+import InputField from "../../../common/fields/InputField/InputField"
+import s from "./FeedbackSection.module.scss"
+import arrow from "../../../../assets/icons/diagonal-arrow.svg"
+import type { FC } from "react"
+
+interface FeedbackSectionProps{
+  image: string
+  h2: string
+  h3: string
+  center?: boolean
+}
+
+const FeedbackSection: FC<FeedbackSectionProps> = ({image, h2, h3, center}) => {
+  return (
+    <div className={`${s["feedback-section"]} ${center && s._center}`} style={{backgroundImage: `url(${image})`, }}>
+      <div className={s["feedback-section__container"]}>
+        <div className={s["feedback-section-label"]}>
+          <h2 className={s["feedback-section-label__h2"]}>{h2}</h2>
+          <h3 className={s["feedback-section-label__h3"]}>{h3}</h3>
+        </div>
+        <div className={s["feedback-section__inputs"]}>
+          <InputField label="Имя"/>
+          <InputField label="Телефон"/>
+        </div>
+        <div className={s["feedback-section__button"]}>
+          <Button variantColor="red" sx={{width: "100%"}}><span>ОТПРАВИТЬ</span><img src={arrow}/></Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedbackSection
